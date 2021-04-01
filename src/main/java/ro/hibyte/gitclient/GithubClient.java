917003b323b569968786236bf18b465a676cae05
Mario Rivis <mario.rivis@gmail.com>
2021-04-01 14:00:23 +0300

    Converted to Kotlin

--- src/main/java/ro/hibyte/gitclient/GithubClient.java
+++
@@ -1,9 +0,0 @@
-package ro.hibyte.gitclient;
-
-import java.util.List;
-
-public class GithubClient {
-    public List<String> getAllBranchesFor(String project) {
-        return List.of("branch1", "branch2", "branch3");
-    }
-}
---
+++ src/main/java/ro/hibyte/gitclient/Main.kt
@@ -0,0 +1,9 @@
+package ro.hibyte.gitclient
+
+fun main() {
+    print("Hello World")
+
+    val githubBranchService: GithubBranchService("dxworks", "github-miner")
+
+}
+
